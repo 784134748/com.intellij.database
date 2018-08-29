a3ab3a4f1f326f40da649531addea7255c2e2c2c
YaLong-Lee <784134748@qq.com>
2018-08-29 21:34:44 +0800

    更新

--- schema/Generate.groovy
+++ schema/Generate.groovy
@@ -336,8 +336,6 @@ def baseXml(out, tableName, className, fields) {
     out.println "    <sql id='query_filter'>"
     fields.each() {
         if (propertiesContainField(it.right, gmtCreate)) {
-        } else if (propertiesContainField(it.right, gmtModified)) {
-            out.println "            ${it.right} = now(),"
         } else {
             out.println "            <if test='${it.left} != null'>${it.right} = #{${it.left}},</if>"
         }
